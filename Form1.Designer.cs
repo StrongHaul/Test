ec26fe7f7ca46dfbb48afef9df0a2e5f68622273
StrongHaul <usermage@gmail.com>
2020-10-23 23:22:16 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Test
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 10);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(120, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(148, 34);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Попыток:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(209, 34);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "label2";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(147, 8);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(97, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Проверить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 33);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(0, 13);
+			this.label3.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(280, 94);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Угадай число";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+	public partial class Form1 : Form
+	{
+		const int maxAttemps = 5;
+		int currentAttempt = maxAttemps;
+		int[] genNumber = new int[4];         // сгенерированное число
+
+		public Form1()
+		{
+			InitializeComponent();
+			label2.Text = currentAttempt.ToString();
+		}
+
+		private void button1_Click(object sender, EventArgs e)		// првоерить
+		{
+			if (textBox1.Text.Length < 4)
+			{
+				MessageBox.Show("Введите четырехзначное число!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			label3.Text = "";
+			var userNumber = textBox1.Text.Select(digit => int.Parse(digit.ToString())).ToArray();     // число пользователя
+
+			if (currentAttempt == maxAttemps)
+			{
+				Array.Copy(GenerateNumber(4, 10), genNumber, 4);
+			}
+
+			if (currentAttempt > 0)
+			{
+				if (NumberMatch(genNumber, userNumber))        // если числа совпадают
+				{
+					label3.Text = $"Угадали!\nЧисло обновлено.";
+					currentAttempt = maxAttemps;
+					return;
+				}
+				Compare(genNumber, userNumber);
+				if (currentAttempt == 1)
+				{
+					label3.Text = "Вы израсходовали попытки.\nЧисло обновлено.";
+					currentAttempt = maxAttemps;
+					label2.Text = currentAttempt.ToString();
+					return;
+				}
+			}
+			currentAttempt--;
+			label2.Text = currentAttempt.ToString();
+		}
+
+		private void textBox1_KeyPress(object sender, KeyPressEventArgs e)      // ввод цифры
+		{
+			var number = e.KeyChar;
+			if (textBox1.Text.Length < 4)
+			{
+				if (!char.IsDigit(number) && number != 8)   // если не число и не backspace
+					e.Handled = true;       // пропустить обработку события
+			}
+			else
+			{
+				if (number != 8)
+					e.Handled = true;
+			}
+		}
+
+		public bool NumberMatch(int[] a, int[] b)		// проверка чисел на полное совпадение
+		{
+			bool match = true;
+			for (int i = 0; i < 4; i++)
+			{
+				if (a[i] != b[i])
+				{
+					match = false;
+					break;
+				}
+			}
+			return match;
+		}
+
+		public void Compare(int[] genNumber, int[] userNumber)      // cравнение чисел
+		{
+			int k = 0;      // количество совпадающих цифр
+			for (int i = 0; i < 4; i++)     // genNumber
+			{
+				for (int j = 0; j < 4; j++)     // userNumber
+				{
+					if (genNumber[i] == userNumber[j])
+					{
+						k++;
+						if (i == j)
+							label3.Text += $"BULL - {userNumber[j]}\n";
+						else
+							label3.Text += $"COW - {userNumber[j]}\n";
+					}
+				}
+			}
+			if (k == 0)
+				label3.Text = "Совпадений не найдено";
+		}
+
+		public int[] GenerateNumber(int numLength, int maxDigit)        // генератор случайного числа
+		{
+			Random random = new Random();
+			int digit, n = 0;
+			var result = new int[4];
+
+			while (n < numLength)
+			{
+				digit = random.Next(maxDigit);
+				bool b = true;
+				for (int i = 0; i < n; i++)
+					if (digit == result[i])
+					{
+						b = false;
+						break;
+					}
+				if (b)
+				{
+					result[n] = digit;
+					n++;
+				}
+			}
+			return result;
+		}
+
+
+	}
+}
